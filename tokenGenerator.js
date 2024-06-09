15815484de408c274e17d106a56a0c10cf85429c
Abir Reza <abir.cse.cou@gmail.com>
2024-06-09 15:20:51 +0200

    Update tokenGenerator.js

--- tokenGenerator.js
+++ tokenGenerator.js
@@ -1,20 +1,14 @@
- const tokenGenerator = function(id) {
+const tokenGenerator = function(id) {
     const Base62 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     const baseSize = Base62.length;
     let tokenarray = [];
-      
-          while (id > 0){
-        
-            Remainder = id % baseSize;
-            tokenarray.push(Base62[Remainder]);
-            id = Math.floor(id / baseSize);
-          }
-        
-     token = tokenarray.join(""); 
-    
-     return token;
-      
+    while (id > 0){        
+      Remainder = id % baseSize;
+      tokenarray.push(Base62[Remainder]);
+      id = Math.floor(id / baseSize);
+    }    
+    token = tokenarray.join(""); 
+    return token;
   }
-
 
 module.exports = tokenGenerator;
